d7c6dfe9861424bbaac0e84a00a497ef7e2de0b2
groupx2 <groupx2021@gmail.com>
2021-04-11 02:50:07 +0530

    dsds

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ app.config(function ($routeProvider,$httpProvider) {
     .when("/contactUs", {
       templateUrl: "contact.html",
       controller: "contactCtrl"
-    })
-    .when("/login",{
-      templateUrl: "login.html",
-      controller: "addLoginController"
     });
 });
 
@@ -143,99 +139,6 @@ app.controller('myCtrl', function ($scope,$http,$compile) {
       alert("unsuccessful call");
       console.log($scope.error);
    });
-  }
-});
-
-
-app.controller('addLoginController',function($scope,$http,$cookies){
-  $scope.login = function() { 
-      $scope.user =  $http({
-            method: 'POST',
-            url: `${url}/api/users/login`,
-           withcredentials: true,
-            data: {
-                email: $scope.email,
-                password: $scope.password
-            },
-            headers:{    
-              'Content-Type': 'application/json'
-          },
-        })
-        .then(function(response){
-            $scope.userData = response.data;
-            $scope.status = response.status;
-            $scope.headers = response.headers;
-            $scope.config = response.config;
-
-          //   $cookies.put("jwt", response.data.token,{
-          //       secure: true,
-          //       samesite: 'None'
-          //   });
-          //  window.location.href = '/welcome.html';
-
-        }, function (response) {
-            $scope.error = response.data;
-            alert("unsuccessful call");
-           console.log($scope.error);
-        });
-}
-
-
-});
-
-app.controller('addSignUpController',function($scope,$http,$log){
-  $scope.signUp = function() { 
-      $scope.user =  $http({
-            method: 'POST',
-            url: 'https://imperial-hotel.herokuapp.com/api/users/signup',
-            data: {
-                name: $scope.name,
-                email: $scope.email,
-                password: $scope.password,
-                passwordConfirm: $scope.passwordConfirm
-            },
-            headers:{    
-              'Content-Type': 'application/json'
-          },
-        })
-        .then(function(response){
-            $scope.userData = response.data;
-            $scope.status = response.status;
-            $scope.headers = response.headers;
-            $scope.config = response.config;
-            window.location.href = '/welcome.html';
-        }, function (response) {
-            $scope.error = response.data;
-            alert("unsuccessful call");
-           console.log($scope.error);
-        });
-  }
-});
-
-
-app.controller('addForgotPasswordController',function($scope,$http,$log){
-  $scope.forgotPassword = function() { 
-      $scope.user =  $http({
-            method: 'POST',
-            url: 'https://imperial-hotel.herokuapp.com/api/users/forgotPassword',
-            data: {
-                email: $scope.email || "test1@gmail.com",
-            },
-            headers:{    
-              'Content-Type': 'application/json'
-          },
-        })
-        .then(function(response){
-            $scope.userData = response.data;
-            $scope.status = response.status;
-            $scope.headers = response.headers;
-            $scope.config = response.config;
-            alert(response.data.message);
-        }, function (response) {
-            $scope.error = response.data;
-            alert("unsuccessful call");
-           console.log($scope.error);
-        });
   }
 });
 
