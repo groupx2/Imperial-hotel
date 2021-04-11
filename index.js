789c9387bbd99315b25b52fb8ea6cee40f85d521
groupx2 <groupx2021@gmail.com>
2021-04-11 17:03:22 +0530

    commit

--- index.js
+++ index.js
@@ -175,12 +175,6 @@ function myHtml(room) {
        <p>${room.description}</p>
        <br>
        <br>
-       <br>
-       <br>
-      <br>
-      <br>
-      <br>
-      <br>
     <h3><P>LKR${room.price} / per night</P> </h3>
    <br>
    <button class="butn" ng-click="bookNow('${room._id}')">
