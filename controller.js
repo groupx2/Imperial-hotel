713fe135af1f34db98a299455e3234e342103b23
groupx2 <groupx2021@gmail.com>
2021-04-14 13:56:40 +0530

    hello

--- controller.js
+++ controller.js
@@ -41,7 +41,7 @@ myModule.controller('addLoginController',function($scope,$http){
 
           }, function (response) {
               $scope.error = response.data;
-              console.log(response.data);
+              console.log(response);
               alert("Please try again later.");
           });
   }
